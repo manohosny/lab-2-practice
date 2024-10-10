cdf423d3e21e4022d632ec9a822358f78cf299da
manohosny <s-abd.khalil@zewailcity.edu.eg>
2024-10-10 20:23:56 +0300

    fix flask errors and enhance the logic of frontend

--- app.py
+++ app.py
@@ -4,21 +4,18 @@ app = Flask(__name__)
 
 users = []
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         gender = request.form.get('gender')
         country = request.form.get('country')
-        
+
         if not all([name, email, gender, country]):
             return render_template('index.html', error="All fields are required!")
-        
+
         user = {
             'name': name,
             'email': email,
@@ -26,14 +23,16 @@ def register():
             'country': country
         }
         users.append(user)
-        
+
         return redirect(url_for('users_list'))
-    
-    return render_template('register.html')
+
+    return render_template('index.html')
+
 
 @app.route('/users')
 def users_list():
     return render_template('users.html', users=users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
